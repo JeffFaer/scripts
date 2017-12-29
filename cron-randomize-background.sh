60644c36531daa4086f79ddb06da688d1e7e8862
Jeffrey Falgout <jeffrey.falgout@gmail.com>
2017-12-29 09:39:05 -0700

    Some shellcheck fixes.

--- cron-randomize-background.sh
+++ cron-randomize-background.sh
@@ -3,12 +3,12 @@
 # Calls randomize-background.sh from cron scripts.
 
 load_environ() {
-    eval $2=$(grep -z "$2" /proc/$1/environ | cut -d= -f2-)
+    eval $2="$(grep -z "$2" "/proc/$1/environ" | cut -d= -f2-)"
     export "$2"
 }
 
 USER=$(whoami)
-PID=$(pgrep -u $USER gnome-session)
-load_environ $PID DBUS_SESSION_BUS_ADDRESS
+PID=$(pgrep -u "$USER" gnome-session)
+load_environ "$PID" DBUS_SESSION_BUS_ADDRESS
 
 randomize-background.sh "$@"
