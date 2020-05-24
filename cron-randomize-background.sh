596143fba0ce9c75f4b5395f0f0144cfa241c6b2
Jeffrey Falgout <jeffrey.falgout@gmail.com>
2020-05-24 14:38:32 -0600

    Pick one of the PIDs if there are more than one.

--- cron-randomize-background.sh
+++ cron-randomize-background.sh
@@ -2,13 +2,15 @@
 
 # Calls randomize-background.sh from cron scripts.
 
+set -x
+
 load_environ() {
     eval $2="$(grep -z "$2" "/proc/$1/environ" | cut -d= -f2-)"
     export "$2"
 }
 
 USER=$(whoami)
-PID=$(pgrep -u "$USER" gnome-session)
+PID=$(pgrep -u "$USER" gnome-session | head -1)
 load_environ "$PID" DBUS_SESSION_BUS_ADDRESS
 
 randomize-background.sh "$@"
