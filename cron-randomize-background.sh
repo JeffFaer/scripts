3f2a338b2600ca349b8a94213311f099b1ca6270
Jeffrey Falgout <jeffrey.falgout@gmail.com>
2020-05-25 13:03:53 -0600

    Fully qualify the path to randomize-backround.sh.

--- cron-randomize-background.sh
+++ cron-randomize-background.sh
@@ -2,8 +2,6 @@
 
 # Calls randomize-background.sh from cron scripts.
 
-set -x
-
 load_environ() {
     eval $2="$(grep -z "$2" "/proc/$1/environ" | cut -d= -f2-)"
     export "$2"
@@ -13,4 +11,4 @@ USER=$(whoami)
 PID=$(pgrep -u "$USER" gnome-session | head -1)
 load_environ "$PID" DBUS_SESSION_BUS_ADDRESS
 
-randomize-background.sh "$@"
+$(dirname "$0")/randomize-background.sh "$@"
