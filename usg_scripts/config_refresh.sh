692a474f9d53cca8964bd60b8174876ab738b79e
Jeffrey Falgout <jeffrey.falgout@gmail.com>
2021-11-28 22:08:53 -0700

    Try actually tweaking something in config_refresh to see if that triggers the config refresh.

--- usg_scripts/config_refresh.sh
+++ usg_scripts/config_refresh.sh
@@ -4,6 +4,12 @@
 source /opt/vyatta/etc/functions/script-template
 
 configure
+
+hostname=( $(show system host-name) )
+
+set system host-name "${hostname[1]}"-tmp
+set system host-name "${hostname[1]}"
+
 commit
 save
 exit
