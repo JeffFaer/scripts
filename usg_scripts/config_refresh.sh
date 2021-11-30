a65f8d9608366d93518bd192f44f46c7e55c6613
Jeffrey Falgout <jeffrey.falgout@gmail.com>
2021-11-29 17:51:41 -0700

    Fix config-refresh.
    I confirmed that this actually does refresh the config.

--- usg_scripts/config_refresh.sh
+++ usg_scripts/config_refresh.sh
@@ -5,11 +5,15 @@ source /opt/vyatta/etc/functions/script-template
 
 configure
 
-hostname=( $(show system host-name) )
+cache_size_entry=( $(show service dns forwarding cache-size) )
+original_cache_size="${cache_size_entry[1]}"
 
-set system host-name "${hostname[1]}"-tmp
-set system host-name "${hostname[1]}"
-
+set service dns forwarding cache-size $((original_cache_size-1))
 commit
 save
+
+set service dns forwarding cache-size ${original_cache_size}
+commit
+save
+
 exit
